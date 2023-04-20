e5d1208be7e29be511f506dc55f88955c56fd200
Lenovo <Lenovo@DESKTOP-D4PVJGF>
2023-04-20 10:39:00 +0200

    Добавьте файлы проекта.

---
+++ Program.cs/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Program.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the cost of notebooks: ");
+            int x = Convert.ToInt32 (Console.ReadLine()); //Стоимость тетради
+            Console.Write("Enter the cost of pens: ");
+            int y = Convert.ToInt32(Console.ReadLine()); //Стоимость ручки
+            Console.Write("Enter the number of notebooks: ");
+            int n = Convert.ToInt32(Console.ReadLine()); //Количество тетрадей
+            Console.Write("Enter the number of pens: ");
+            int m = Convert.ToInt32(Console.ReadLine()); //Количество ручек
+            int totalAmount = x*n + y*m;
+            Console.WriteLine($"Total amount of purchase: {totalAmount}");
+        }
+    }
+}
